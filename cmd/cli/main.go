4e2237105d6b93a43d3b309d21aecdff7cc119b2
Kyour <kyour@outlook.com>
2025-05-04 14:08:50 +0800

    chore: 添加 CLI 主入口文件
    - 新建 cmd/cli/main.go 文件
    - 实现应用程序初始化和事件触发逻辑
    - 进入任务启动状态并等待

---
+++ cmd/cli/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "app/internal/initialize"
+    "context"
+    "github.com/go-gourd/gourd/event"
+)
+
+func main() {
+
+    // 初始化
+    initialize.InitApp()
+
+    event.Trigger("task.start", context.Background())
+
+    select {}
+}
