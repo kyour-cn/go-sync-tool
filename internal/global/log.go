d3f80e7e5696d43088368a0200f52d034ede5d69
Kyour <kyour@outlook.com>
2025-05-13 09:21:31 +0800

    refactor(log): 优化日志记录逻辑
    - 将日志插入位置改为头部，以保持最新日志在前
    - 修改日志截断逻辑，保留前100条（包括新插入的日志）
    - 移除了注释的代码，简化了逻辑

--- internal/global/log.go
+++ internal/global/log.go
@@ -17,24 +17,18 @@ var Logs []Log
 // WriteConsoleLog 写入日志到控制台
 func WriteConsoleLog(l string, msg string) {
 
-	//判断日志仅保留最后100条
-	if len(Logs) > 100 {
-		Logs = Logs[len(Logs)-100:]
-	}
-	//Logs = append(Logs, Log{
-	//    Time:    time.Now(),
-	//    Level:   "info",
-	//    Message: string(p),
-	//})
-
-	// 往头部插入数据
+	// 往头部插入新日志
 	Logs = append([]Log{{
 		Time:    time.Now(),
 		Level:   l,
 		Message: msg,
 	}}, Logs...)
 
+	// 截断保留前100条（新插入的日志在头部）
+	if len(Logs) > 100 {
+		Logs = Logs[:100]
+	}
+
 	// 刷新日志UI
 	event.Trigger("window.invalidate", context.Background())
-
 }
