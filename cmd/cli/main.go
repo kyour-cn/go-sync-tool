99b4aff5b7665275bcbc1e7f3221280dbf3a769d
Kyour <kyour@outlook.com>
2025-05-15 10:49:51 +0800

    docs(cli): 添加命令行无 UI 版本注释说明

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,11 +6,13 @@ import (
 	"github.com/go-gourd/gourd/event"
 )
 
+// 命令行无UI版本，可编译至linux系统
 func main() {
 
 	// 初始化
 	initialize.InitApp()
 
+	// 触发任务开始
 	event.Trigger("task.start", context.Background())
 
 	select {}
