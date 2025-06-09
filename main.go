38705913a9961dd5a4cce21f57500d4f55c3cd01
Kyour <kyour@outlook.com>
2025-06-09 16:44:05 +0800

    feat(main): 添加程序单实例锁
    - 使用 Windows 互斥体 (Mutex) 机制确保程序只运行一个实例
    - 在程序启动时创建互斥体，如果已存在则退出
    - 程序退出时释放互斥体资源

--- main.go
+++ main.go
@@ -4,14 +4,40 @@ import (
 	"app/internal/domain"
 	"app/internal/initialize"
 	mainApp "app/ui/app"
+	"errors"
+	"fmt"
 	"gioui.org/app"
 	"gioui.org/unit"
+	"golang.org/x/sys/windows"
 	"log"
 	"os"
+	"syscall"
 )
 
 //go:generate rsrc -ico assets/images/favicon.ico -manifest assets/app.manifest -o main.syso
 func main() {
+
+	// 创建一个具有唯一名称的互斥体
+	mutexName := "Global\\JxSyncUtilV3"
+	mutex, err := windows.CreateMutex(nil, false, windows.StringToUTF16Ptr(mutexName))
+	if err != nil {
+		fmt.Println("Error creating mutex:", err)
+		return
+	}
+	if errCode := syscall.GetLastError(); errors.Is(errCode, windows.ERROR_ALREADY_EXISTS) {
+		fmt.Println("Another instance of the program is already running.")
+		return
+	} else if errCode != nil {
+		fmt.Println("Error checking for existing instance:", errCode)
+		return
+	}
+	// 在程序退出时释放互斥体资源
+	defer func(handle windows.Handle) {
+		err := windows.CloseHandle(handle)
+		if err != nil {
+			os.Exit(1)
+		}
+	}(mutex)
 
 	// 初始化
 	initialize.InitApp()
